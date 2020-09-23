1ed1bf83f64170a7e2994b8163a1b51281e2ba9c
fuzz503 <54230932+fuzz503@users.noreply.github.com>
2020-09-23 18:56:13 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -273,7 +273,7 @@ def infinite(pn, dl, ch, max):
             continue
         os.system('rm proc.xxx >/dev/null 2>&1')
         count_inf += 1
-        # os.system('echo SpeedX >> count.xxx')
+        # os.system('echo Fuzz503 >> count.xxx')
         time.sleep(float(dl))
         if (count_inf > maxlim):
             exit()
@@ -740,7 +740,7 @@ def start(target, counter, delay, ch, cc):
         print("             Failed Requests         : ", failed)
         print("==================================================================")
         print("              Use this for fun, not for revenge !!                ")
-        print("              This Bomber Was Created By SpeedX !!                ")
+        print("              This Bomber Was Created By PranithVarma !!          ")
         print("==================================================================")
         try:
             result = getapi(target, api, cc)
@@ -906,7 +906,7 @@ if nm == 0:
     print("             Delay               : ", dl)
     print("==================================================================")
     print("              Use this for fun, not for revenge !!                ")
-    print("              This Bomber Was Created By SpeedX !!                ")
+    print("              This Bomber Was Created By PranithVarma !!          ")
     print("==================================================================")
     print(W)
     input('\n\nPress CTRL+Z To STOP Bomber... \nPress Enter To Start Bomber...\n')
