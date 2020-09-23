0496fa143f31832fbdb475d5b3dfd598352b96a2
fuzz503 <54230932+fuzz503@users.noreply.github.com>
2020-09-23 19:08:04 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -26,7 +26,7 @@ clear
 echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border Pranith
 echo -e "\e[4;34m This Bomber Was Created By PranithVarma \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
 echo -e "\e[1;32m           Mail: appanipranith@gmail.com \e[0m"
